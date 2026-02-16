715c569c20aa5ad06a4c7d69c209591b44c6801a
Elie Martila -- van Beek <elie.vanbeek66@gmail.com>
2026-02-16 15:00:44 +0100

    fix : correct layout issues on small/large screens on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,11 +31,37 @@ export default function Contact () {
     return (
         <div className="h-screen bg-blue-100">
             <TopMenu/>
-            <h1 className="py-15 flex justify-center sm:text-7xl text-5xl">Me Contacter :</h1>
-            <p className="sm:hidden text-center">(Contact affiché en bas de page)</p>
-            <div className="bg-blue-100 w-full">
-                <div className="2xl:ml-55 2xl:mr-55 xl:ml-25 xl:mr-25 grid sm:grid-cols-3 lg:gap-40 
-                                lg:ml-20 lg:mr-20 md:ml-10 md:mr-10 md:gap-20 ml-25 
+            <h1 className="2xl:mt-20 xl:mt-15 lg:mt-17 md:mt-20 sm:mt-20 flex justify-center 
+                            2xl:text-7xl sm:text-5xl">
+                Me Contacter :
+            </h1>
+            <div className={` flex justify-center mb-5 
+                            2xl:text-4xl lg:text-3xl bg-blue-100 sm:text-2xl text-md`}>
+                <div className={`${showContact ? "" : "w-2/3"} sm:w-2/3 xl:w-2/5 border-black border-4 rounded-md flex 
+                                justify-center bg-slate-400 py-2 sm:py-0`}>
+                    {showContact === "ig" && <p>@elie_mvb</p>}
+                    {showContact === "mail" && <p>elie.vanbeek66@gmail.com</p>}
+                    {showContact === "in" && <p>IN</p>}
+                    {showContact === null && <p>---</p>}
+                    {showContact != null && 
+                        <button onClick={handleCopy} className="ml-4 h-[50px] 2xl:mt-10 sm:mt-6 grid grid-cols-2
+                                                        bg-white rounded-lg">
+                            {!copiedImage && <>
+                                    <p className="w-[50px]">Copier</p>
+                                    <img src={copy} className="w-[30px] mt-2 ml-2"/>
+                                </>}
+                            {copiedImage && <>
+                                    <p className="w-[50px]">Copié !</p>
+                                    <img src={copied} className="w-[30px] mt-2 ml-2"/>
+                                </>}
+                        </button>
+                    
+                    }
+                </div>
+            </div>
+            <div className="bg-blue-100 w-full mb-4">
+                <div className="2xl:ml-55 2xl:mr-55 xl:ml-40 xl:mr-40 grid sm:grid-cols-3 lg:gap-40 
+                                lg:ml-20 lg:mr-20 md:ml-30 md:mr-30 md:gap-25 ml-25 
                                 mr-25 gap-5 grid-cols-1">
                     <button className="aspect-square shadow-2xl bg-white border-transparent 
                                         rounded-2xl hover:brightness-75" 
@@ -53,26 +79,6 @@ export default function Contact () {
                         <img src={linkedin} className="w-[80%]"/>
                     </button>
                 </div>
-            </div>
-            <div className={`${showContact ? "flex" : "hidden"} sm:mt-10 justify-center sm:text-4xl 
-                            bg-blue-100 text-2xl`}>
-                {showContact === "ig" && <p>@elie_mvb</p>}
-                {showContact === "mail" && <p>elie.vanbeek66@gmail.com</p>}
-                {showContact === "in" && <p>IN</p>}
-                {showContact != null && 
-                    <button onClick={handleCopy} className="ml-4 h-[50px] sm:mt-10 mt-5 grid grid-cols-2
-                                                    bg-white rounded-lg">
-                        {!copiedImage && <>
-                                <p className="w-[50px]">Copier</p>
-                                <img src={copy} className="w-[30px] mt-2 ml-2"/>
-                            </>}
-                        {copiedImage && <>
-                                <p className="w-[50px]">Copié !</p>
-                                <img src={copied} className="w-[30px] mt-2 ml-2"/>
-                            </>}
-                    </button>
-                
-                }
             </div>
         </div>
     )
