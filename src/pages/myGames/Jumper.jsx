c421596b124dd3433d93ebe4b616221b0a997683
Elie Martila -- van Beek <elie.vanbeek66@gmail.com>
2026-03-09 09:00:35 +0100

    fix/fixing some size for phone screen size

--- src/pages/myGames/Jumper.jsx
+++ src/pages/myGames/Jumper.jsx
@@ -8,38 +8,38 @@ export function Jumper () {
     return (
             <div className="bg-indigo-950 text-white">
                 <TopMenu/>
-                <div className="flex flex-row justify-center">
-                    <img className="w-[150px] h-[150px] mr-10" src={player}/>
-                    <h1 className="text-center mt-20 text-amber-300 sm:text-7xl text-5xl">Jumper</h1>
+                <div className="flex flex-row justify-center sm:mt-5">
+                    <img className="sm:w-[150px] sm:h-[150px] mr-10 w-[90px] h-[90px] mt-10 sm:mt-0" src={player}/>
+                    <h1 className="text-center mt-20 text-amber-300 sm:text-7xl sm:text-5xl text-3xl">Jumper</h1>
                 </div>
                 <h1 className="ml-6">Le jeux :</h1>
-                <h2 className="ml-8">
+                <h2 className="ml-8 sm:text-2xl text-lg">
                     Ce jeux a été codé en 6h avec un ami lors de la Nuit du Code 2023.
                 </h2>
-                <h2 className="ml-8">
+                <h2 className="ml-8 sm:text-2xl text-lg">
                     J'aimerais dire qu'on l'avait gagnée, car parmis les lycées de Perpignan c'est
                     bien le cas. Mais je crois que notre prof n'a jamais fait les démarches
                     pour réellement participer à la "vraie" Nuit du Code internationale...
                 </h2>
-                <h2 className="ml-8">
+                <h2 className="ml-8 sm:text-2xl text-lg">
                     Qu'importe, beaucoup de plaisir à développer ce petit jeux sous la pression
                     de la compétition. Ce que j'aime réellement beaucoup c'est qu'on a créé une
                     sorte de petit moteur de jeux et qu'il est possible de rajouter un niveau sans
                     devoir réellement coder en python. Par contre, il faudrait décoder comment
                     on a choisi de stocker les niveaux car je n'en ai pas beaucoup de souvenirs...
                 </h2>
-                <h1 className="ml-6 mt-12">Petite vidéo de gamplay :</h1>
+                <h1 className="ml-6 mt-12 sm:text-3xl text-lg">Petite vidéo de gamplay :</h1>
                 <div className="flex justify-center">
                     <video src={video} width="500" height="500" controls="controls" className="sm:block hidden"/>
                     <video src={video} width="500" height="500" controls="controls" className="sm:hidden w-[75%]"/>
                 </div>
-                <h1 className="ml-6 py-8">
+                <h1 className="ml-6 py-8 sm:text-3xl text-lg">
                     Photo de la victoire :
                 </h1>
                 <div className="flex">
-                    <img src={ndc} className="xl:ml-100 lg:ml-75 sm:ml-30"/>
+                    <img src={ndc} className="xl:ml-100 lg:ml-75 sm:ml-30 w-[90%] sm:w-[50%]"/>
                 </div>
-                <h1 className="ml-6 py-8">
+                <h1 className="ml-6 py-8 sm:text-3xl text-lg">
                     Télécharcger le jeux au format .pyxapp :
                     <button className="ml-8 text-2xl hover:bg-emerald-600
                                 bg-emerald-500 rounded-lg border-emerald-700">
@@ -49,10 +49,11 @@ export function Jumper () {
                         </a>
                     </button>
                 </h1>
-                <h1 className="ml-6 mb-4">
+                <h1 className="sm:ml-6 ml-2 mb-4 sm:text-3xl text-lg">
                     Lien GitHub du projet :
+                    <br/>
                     <a href="https://github.com/ElieMvB/Jumper" target="_blank"
-                    className="ml-8 text-amber-400 hover:text-amber-600 text-2xl">
+                    className="sm:ml-8 text-amber-400 hover:text-amber-600 sm:text-2xl text-base">
                         https://github.com/ElieMvB/Jumper
                     </a>
                 </h1>
--- src/pages/myGames/Selda.jsx
+++ src/pages/myGames/Selda.jsx
@@ -9,25 +9,25 @@ export function Selda () {
             <div>
                 <h1 className="text-center mt-20 text-rose-600 text-7xl">Selda</h1>
             </div>
-            <h1 className="ml-6">Le jeux :</h1>
-            <h2 className="ml-8">
+            <h1 className="sm:ml-6 ml-2 sm:text-3xl text-xl">Le jeux :</h1>
+            <h2 className="sm:ml-8 ml-3 sm:text-2xl text-lg">
                 Le jeux a été fait avec l'aide de Manoé (mon frère). Il a fait les textures et
                 toutes les musiques originales (celle qui sont des reprises de titres existants
                 ont étés codées par moi).
                 Sans surprise, le jeux s'inspire fortement de Zelda, quoi qu'il soit plus simple
                 que le premier Zelda sortit...
             </h2>
-            <h2 className="ml-8">
+            <h2 className="sm:ml-8 ml-3 sm:text-2xl text-lg">
                 L'objectif est simplement de libérer un chateau envahit par des monstres.
             </h2>
-            <h1 className="ml-6 mt-12">Petite vidéo de gamplay :</h1>
+            <h1 className="sm:ml-6 ml-2 mt-12 sm:text-3xl text-xl">Petite vidéo de gamplay :</h1>
             <div className="flex justify-center">
                 <video src={video} width="500" height="500" controls="controls" 
                     className="sm:block hidden"/>
                 <video src={video} width="500" height="500" controls="controls" 
                     className="sm:hidden w-[75%]"/>
             </div>
-            <h1 className="ml-6 py-8">
+            <h1 className="sm:ml-6 ml-2 py-8 sm:text-3xl text-xl">
                 Télécharcger le jeux au format .pyxapp :
                 <button className="ml-8 text-2xl hover:bg-emerald-600
                             bg-emerald-500 rounded-lg border-emerald-700">
@@ -37,10 +37,11 @@ export function Selda () {
                     </a>
                 </button>
             </h1>
-            <h1 className="ml-6 mb-0">
+            <h1 className="sm:ml-6 ml-2 mb-0 sm:text-3xl text-xl">
                 Lien GitHub du projet :
+                <br/>
                 <a href="https://github.com/ElieMvB/Galaxian" target="_blank"
-                className="ml-8 text-amber-600 hover:text-amber-800 text-2xl">
+                className="sm:ml-8 text-amber-600 hover:text-amber-800 sm:text-2xl text-base">
                     https://github.com/ElieMvB/Galaxian
                 </a>
             </h1>
