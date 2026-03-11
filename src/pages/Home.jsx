68f6a367ca46de54d34f8ed6d377863bf4949875
Elie Martila -- van Beek <elie.vanbeek66@gmail.com>
2026-03-11 20:09:46 +0100

    wip: add slide bare home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,52 @@
 import { MainBanner } from "../components/banners/MainBanner"
 import {TopMenu} from "../components/TopMenu"
 import { GoTopButton } from "../components/GoTopButton"
+import { useState } from "react"
+import le36 from "../assets/images/36.jpg"
+import front from "../assets/images/front.jpg"
+
+function SlideMenu () {
+  const [ind, setInd] = useState(0);
+  const img = [le36, front];
+
+  function rightButtonClick () {
+    console.log("right");
+    if (ind == img.length - 1) {
+      setInd(0);
+    } else {
+      setInd(ind + 1);
+    }
+  }
+
+  function leftButtonClick () {
+    console.log("left");
+    if (ind == 0) {
+      setInd(img.length - 1);
+    } else {
+      setInd(ind - 1);
+    }
+  }
+
+  return (
+    <div className="bg-slate-400 shadow-lg">
+      <div className="grid grid-cols-4 flex">
+        <div className="col-span-1 col-start-1 flex center justify-center">
+          <button 
+            onClick={leftButtonClick}
+            className="h-[100%]">
+            Tourne à gauche
+            </button>
+        </div>
+        <div className="col-span-2 col-start-2 flex center justify-center items-center">
+          <img className="w-[80%] object-contain h-[80%]" src={img[ind]} />
+        </div>
+        <div className="col-span-1 col-start-4 flex center justify-center">
+          <button onClick={rightButtonClick}>Tourne à droite</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function Home () {
   return (
@@ -15,7 +61,9 @@ function Home () {
         ou autre.
       </h2>
     </div>
-    <div style={{height: '50vh'}}></div>
+    <div style={{height: '10vh'}}></div>
+    <SlideMenu/>
+    <div style={{height: '20vh'}}></div>
   </div>
   )
 }
