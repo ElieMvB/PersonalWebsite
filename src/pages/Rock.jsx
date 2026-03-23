3de3554072539265bd13e51f3dbbeacbdc9c89d1
Elie Martila -- van Beek <elie.vanbeek66@gmail.com>
2026-03-23 13:45:43 +0100

    fix: fixing responsivness of 'rock' page

--- src/pages/Rock.jsx
+++ src/pages/Rock.jsx
@@ -5,18 +5,18 @@ export default function Rock () {
     return (
         <div className="bg-slate-700 h-full">
             <TopMenu/>
-            <div className="h-20"/>
+            <div className="h-20 hidden sm:block"/>
             <div className="flex justify-center text-shadow-2xs text-shadow-slate-500">
-                <div className="bg-slate-900 w-[70%]">
+                <div className="bg-slate-900 sm:w-[70%] w-[100%]">
                     <div className="text-red-600 text-center text-3xl">
                         <h1>Let me dance</h1>
                         <h2>like a bug on a cinema screen !</h2>
                     </div>
-                    <div className="text-green-700 ml-10 text-xl">
-                        <h1>
+                    <div className="text-green-600 text-xl">
+                        <h1 className="md:ml-10 ml-5">
                             Ragda !
                         </h1>
-                        <p className="ml-5">
+                        <p className="md:ml-15 ml-7">
                             Spectacle étudiant après seulement 4 mois et demi de dance.
                             <br/>Un vrai banger, on s'est bien amusés !
                         </p>
@@ -28,13 +28,13 @@ export default function Rock () {
                             />
                         </div>
                     </div>
-                    <div className="text-red-700 text-xl ml-10">
-                        <h1 className="text-white">Chorées des 24h de <strong className="text-red-700">l'INSA</strong> !</h1>
+                    <div className="text-red-700 text-xl">
+                        <h1 className="text-white md:ml-10 ml-5">Chorées des 24h de <strong className="text-red-700">l'INSA</strong> !</h1>
                         <p className="text-slate-400 text-center">Comming soon</p>
                     </div>
-                    <div className="text-amber-400 text-xl ml-10">
-                        <h1>If's got talent</h1>
-                        <p>
+                    <div className="text-amber-400 text-xl">
+                        <h1 className="md:ml-10 ml-5">If's got talent</h1>
+                        <p className="md:ml-15 ml-7">
                             Petite chorée réalisée avec 3 amis danceurs. <br/>
                             Contrairement aux deux autres chorées, on n'avait pas de 
                             chorégraphe : on s'est donc improvisés chorégraphes tous ensembles !
@@ -43,6 +43,7 @@ export default function Rock () {
                     </div>
                 </div>
             </div>
+            <div className="h-20 hidden sm:block"/>
         </div>
     )
 };
